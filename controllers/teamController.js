dd764f055d4a4287539d9efd53d06b0d23fbb332
jedgodsey <jed.godsey@gmail.com>
2020-10-09 00:04:46 -0700

    deleted dead route

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
-// const teams = require('../models/teams');
 
 ////// current paths imply '/teams'
 
